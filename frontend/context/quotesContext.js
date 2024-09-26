5ba3bc6cf33bf19fe1f6210837534ac737820b96
francesco damore <frandamore11@gmail.com>
2024-09-25 22:02:06 -0600

    created context

--- frontend/context/quotesContext.js
+++ frontend/context/quotesContext.js
@@ -1,1 +1,19 @@
 // Create your context here
+import React, {createContext}  from "react";
+
+export const quotesContext = createContext()
+
+const value = { 
+    car: 'Toyota Celica'
+}
+
+export function QuotesProvider(props) {
+
+    return (
+        <quotesContext.Provider value ={value}>
+            {props.children}
+        </quotesContext.Provider>
+    )
+}
+
+
